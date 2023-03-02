41387d64f11229c798a0bf714c719485770b3f7a
is13126 <is13126@B206-2>
2023-03-02 10:45:22 +0700

    xcz

--- WindowsFormsApp8/flights.Designer.cs
+++ WindowsFormsApp8/flights.Designer.cs
@@ -66,6 +66,7 @@ namespace WindowsFormsApp8
             this.button2.TabIndex = 5;
             this.button2.Text = "отчистить";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
--- WindowsFormsApp8/flights.cs
+++ WindowsFormsApp8/flights.cs
@@ -12,9 +12,16 @@ namespace WindowsFormsApp8
 {
     public partial class flights : Form
     {
+        private Flightsdatabase _flight;
         public flights()
         {
             InitializeComponent();
+            
+
+            _flight = new Flightsdatabase();
+            _flight.Initialize();
+
+            dataGridView1.DataSource = _flight.flighttables.ToList();
         }
 
         private void flights_Load(object sender, EventArgs e)
@@ -53,6 +60,12 @@ namespace WindowsFormsApp8
             this.Visible = false;
             Tours newform = new Tours();
             newform.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = _flight.flighttables.ToList();
         }
     }
 }
