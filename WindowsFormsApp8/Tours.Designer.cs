780672e6c0f22d00c6b33cb616ec773580ee985d
is13126 <is13126@B206-2>
2023-03-03 15:02:12 +0700

    uigrtydtyr678576567

--- WindowsFormsApp8/Tours.Designer.cs
+++ WindowsFormsApp8/Tours.Designer.cs
@@ -54,6 +54,8 @@ namespace WindowsFormsApp8
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(27, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(540, 169);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
