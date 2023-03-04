06a12b124079b172f15d5c7d6a6ed8962c005ac6
is13126 <is13126@B206-2>
2023-03-04 11:35:23 +0700

    w1qeq`2312313

--- WindowsFormsApp8/Airoportsdatabase.cs
+++ WindowsFormsApp8/Airoportsdatabase.cs
@@ -8,5 +8,20 @@ namespace WindowsFormsApp8
 {
     class Airoportsdatabase
     {
+        internal List<travel_maptabl> travel_Maptabls { get; private set; }
+        public void Initialize()
+        {
+            travel_Maptabls = new List<travel_maptabl>()
+            {
+
+                new travel_maptabl(new Point("Москва"),new Point("Екатеринбург"),new Point("Санкт-Петербург")),
+                new travel_maptabl(new Point("Новосибирск"),new Point("Казань"),new Point("Москва"))
+
+
+
+
+
+            };
+        }
     }
 }
